1865cba19a966ae906dec98f6f51dc9b8aca22cb
Iri O <oparina.iri@gmail.com>
2024-03-12 19:41:49 +0500

    Added encoders file

---
+++ encoders.py
@@ -0,0 +1,10 @@
+
+from json import JSONEncoder
+from monster import Monster
+
+
+class MonsterJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Monster):
+            return vars(obj)
+        return JSONEncoder.default(self, obj)
