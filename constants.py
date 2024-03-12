da909b8c2b70a94f0a013778e88a390612da13e6
Iri O <oparina.iri@gmail.com>
2024-03-12 19:41:26 +0500

    Added constants file

---
+++ constants.py
@@ -0,0 +1,9 @@
+
+MONSTER_FILE = 'monsters.json'
+MONSTER_FILE_URL = 'https://raw.githubusercontent.com/Misha-creato/dungeon/main/monsters.json'
+SAVE_FILE = 'save.json'
+MIN_EXPERIENCE_POINTS = 10
+MAX_EXPERIENCE_POINTS = 50
+RUN_ENERGY_POINTS = -3
+FIGHT_ENERGY_POINTS = -10
+
