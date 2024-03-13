e297bc82e7a2cad3b306044a11c6784ecd3a7139
Iri O <oparina.iri@gmail.com>
2024-03-13 05:47:33 +0500

    get_player, fix_save_file_settings

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ from constants import (
 )
 
 
+def fix_save_file_settings(interface: Interface):
+    interface.print_save_file_error(file=SAVE_FILE)
+    interface.player_menu_options.pop('2')
+
+
 def get_monsters_file():
     response = requests.get(MONSTER_FILE_URL)
     data = response.json()
@@ -29,20 +34,32 @@ def fix_file_settings(interface: Interface):
         get_monsters_file()
 
     if not check.is_file_available(file=SAVE_FILE):
-        interface.print_save_file_error(file=SAVE_FILE)
-        interface.player_menu_options.pop('2')
+        fix_save_file_settings(interface=interface)
+
+
+def get_player(interface: Interface):
+    save_loaded = False
+    while not save_loaded:
+        answer = interface.get_user_answer(interface.player_menu_options)
+        if answer == '1':
+            player = Player()
+            save_loaded = True
+        elif answer == '2':
+            player = Player()
+            save_loaded = player.load_player_data()
+            if not save_loaded:
+                fix_save_file_settings(interface=interface)
+        else:
+            interface.print_results()
+            sys.exit(0)
+    return player
 
 
 def main():
     interface = Interface()
     interface.greeting()
     fix_file_settings(interface)
-    answer = interface.get_user_answer(interface.player_menu_options)
-    player = Player()
-    if answer == '3':
-        sys.exit(0)
-    elif answer == '2':
-        player.load_player_data()
+    player = get_player(interface=interface)
     game = Game(player=player, interface=interface)
     game.start_game()
 
