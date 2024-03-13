93f57ef89f9d5c798ab88d704a807c01ccdedb68
Iri O <oparina.iri@gmail.com>
2024-03-13 05:44:08 +0500

    Changed values of constants

--- check.py
+++ check.py
@@ -5,3 +5,4 @@ def is_file_available(file: str):
     if os.path.isfile(file) and os.path.getsize(file) > 0:
         return True
     return False
+
--- constants.py
+++ constants.py
@@ -1,9 +1,8 @@
-
 MONSTER_FILE = 'monsters.json'
 MONSTER_FILE_URL = 'https://raw.githubusercontent.com/Misha-creato/dungeon/main/monsters.json'
 SAVE_FILE = 'save.json'
 MIN_EXPERIENCE_POINTS = 10
 MAX_EXPERIENCE_POINTS = 50
-RUN_ENERGY_POINTS = -3
-FIGHT_ENERGY_POINTS = -10
+RUN_ENERGY_POINTS = 3
+FIGHT_ENERGY_POINTS = 10
 
